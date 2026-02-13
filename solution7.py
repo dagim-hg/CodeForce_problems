6cafcc17fac86f9e978e4c261b56d812761e7b8b
dagim-hg <kenadagi430@gmail.com>
2026-02-13 12:29:18 +0300

    Simplify string processing for chat decision
    Refactor string uniqueness check using set.

--- solution7.py
+++ solution7.py
@@ -1,8 +1,4 @@
-string = input("Enter a string: ")
-new_string = ""
-for char in string:
-  if char not in new_string:
-    new_string += char
-if len(new_string)%2 == 0:
+string = set(list(input("Enter a string: ")))
+if len(string)%2 == 0:
   print("CHAT WITH HER!")
 else:  print("IGNORE HIM!")
